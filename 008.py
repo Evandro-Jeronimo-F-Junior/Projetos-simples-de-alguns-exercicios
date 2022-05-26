f8235ed266a0e521b5aba93f976c8f69e039bdc1
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2022-05-26 11:51:23 -0300

    Conversor de medidas

---
+++ 008.py
@@ -0,0 +1,2 @@
+metros = float(input('Digite um valor em metros: '))
+print('Metros: {}\nKilometros: {}\nHectometros: {}\nDecametros: {}\nDecimetros: {}\nCentímetros: {}\nMilímetros: {}'.format(metros, metros / 1000, metros / 100, metros / 10, metros * 10, metros * 100, metros * 1000))
