e7866621da72d305b36e073765b66e8668ef6953
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2022-05-26 11:49:06 -0300

    Média de nota de 4 bimestres

---
+++ 007.py
@@ -0,0 +1,6 @@
+print('Para saber a média dos alunos digite as quatros notas ou menos')
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+n3 = float(input('Terceira nota: '))
+n4 = float(input('Quarta nota: '))
+print('Sua média é {:.1f}'.format((n1 + n2 +n3 +n4) / 4))
