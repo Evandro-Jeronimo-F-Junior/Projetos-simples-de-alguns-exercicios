f8d2c7e7f9f651d6e3cf908a36d4e56d97141539
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2022-06-06 18:00:30 -0300

    um mini caixa eletronico, mas o do professor ficou bom demais, então deixei o do professor

---
+++ 071.py
@@ -0,0 +1,19 @@
+valor = int(input('Digite o valor a ser sacado: '))
+ced = 50
+totced = 0
+while True:
+    if valor >= ced:
+        valor -= ced
+        totced += 1
+    else:
+        if totced>0:
+            print(f'Você sacou {totced} notas de {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if valor == 0:
+            break
