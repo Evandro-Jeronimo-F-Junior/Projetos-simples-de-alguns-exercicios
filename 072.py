e83a24cfd7ef0fc50ca62168f2a45244d95a704b
Evandro-Jeronimo-F-Junior <evandro.jeronimo.f.junior@gmail.com>
2022-06-07 13:54:16 -0300

    uso simples de uma tupla

---
+++ 072.py
@@ -0,0 +1,2 @@
+ne = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'douze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+print(ne[int(input('Digite um número de 1 a 20 para saber ele por extenso: '))])
